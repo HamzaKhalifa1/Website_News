ce2e6f45e23d7c1749081b9f2463926baa37848b
TODO list <engHamzaKha@gmail.com>
2024-12-03 09:56:13 +0200

    Final version (without errors in running the project)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Header from "./Componant/Header/index";
 import Main_title from "./Componant/Main_Titel/index";
 import Footer from "./Componant/Footer/index"
 import HomePage from "./Pages/HomePage/index";
-=======
-import Header from "./Componant/Header/Header";
-import Main_title from "./Componant/Main_Titel/Main_title";
-import Footer from "./Componant/Footer/Footer"
-import Over_all from "./Componant/container/container";
->>>>>>> 77e5675db8ce2eaa2b38c9b91916846f68aaeefb
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main_title/>
-<<<<<<< HEAD
             <HomePage/>
-=======
-            <Over_all/>
->>>>>>> 77e5675db8ce2eaa2b38c9b91916846f68aaeefb
             <Footer/>
         </div>
     );
--- src/Componant/Footer/Footer.tsx
+++ src/Componant/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import styles from './Footer.module.css'
 
 export default function Footer(){
@@ -20,22 +19,6 @@ export default function Footer(){
                 </div>
                 <div><p className={styles.Large_text_footer}><i className="fa-solid fa-phone"></i>0597956633</p></div>
                 <div><p className={styles.Large_text_footer}><i className="fa-solid fa-house"></i>Jenin/Haifa Street</p></div>
-=======
-import './Footer.css'
-
-export default function Footer(){
-    return (
-        <footer id="footer">
-            <div id="footer_sub">
-                <div><p className="Large_text_footer"><i className="fa-solid fa-user"></i>Hamza Khalifa</p></div>
-                <div><a href="https://www.linkedin.com/in/hamza-khalifa-385b23287" target="_blank"><p
-                    className="Large_text_footer"><i className="fa-brands fa-linkedin"></i>LinkedIn</p></a></div>
-                <div><a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox" target="_blank"><p
-                    className="Large_text_footer"><i className="fa-solid fa-envelope"></i>engHamzaKha@gmail.com</p></a>
-                </div>
-                <div><p className="Large_text_footer"><i className="fa-solid fa-phone"></i>0597956633</p></div>
-                <div><p className="Large_text_footer"><i className="fa-solid fa-house"></i>Jenin/Haifa Street</p></div>
->>>>>>> 77e5675db8ce2eaa2b38c9b91916846f68aaeefb
             </div>
         </footer>
     );
