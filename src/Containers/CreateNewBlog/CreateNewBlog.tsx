c62154adbfe02a61ca61650461e650c95ad0fbe9
HamzaKhalifa1 <enghamzakha@gmail.com>
2024-12-21 20:15:02 +0200

    Update folder name

---
+++ src/Containers/CreateNewBlog/CreateNewBlog.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import './CreateNewBlog.css';
+import { useForm } from 'react-hook-form';
+
+const CreateNewBlog = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data: any) => console.log(data);
+
+    return (
+        <form id="new-blog-form" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="title">Title:</label>
+            <input
+                type="text"
+                id="title"
+                className={`title-input ${errors.title ? "error" : ""}`}
+                placeholder="Enter title"
+                {...register("title", { required: true, pattern: /^[A-Z]/, maxLength: 50 })}
+            />
+            <div id="title-error" className="error-message">
+                {errors.title?.type === "required" && "Title is required."}
+                {errors.title?.type === "pattern" && "Title must start with a capital letter."}
+                {errors.title?.type === "maxLength" && "Title cannot exceed 50 characters."}
+            </div>
+
+            <label htmlFor="description">Description:</label>
+            <textarea
+                id="description"
+                className={`textarea-input ${errors.description ? "error" : ""}`}
+                placeholder="Enter description"
+                {...register("description", { required: true, pattern: /^[a-zA-Z ]*$/, maxLength: 1000 })}
+            ></textarea>
+            <div id="description-error" className="error-message">
+                {errors.description?.type === "required" && "Description is required."}
+                {errors.description?.type === "pattern" && "Description can only contain letters and spaces."}
+                {errors.description?.type === "maxLength" && "Description cannot exceed 1000 characters."}
+            </div>
+
+            <label htmlFor="image-url">Image URL:</label>
+            <input
+                type="text"
+                id="image-url"
+                className={`image-input ${errors.imageUrl ? "error" : ""}`}
+                placeholder="Enter image URL"
+                {...register("imageUrl", { pattern: /^(https?:\/\/[^\s]+)$/ })}
+            />
+            <div id="image-url-error" className="error-message">
+                {errors.imageUrl?.type === "pattern" && "Please enter a valid URL."}
+            </div>
+
+            <input type="submit" />
+        </form>
+    );
+}
+
+export default CreateNewBlog;
