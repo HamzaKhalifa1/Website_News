5048925431ba4e3af6b7f2c127ba632d5b2effaa
HamzaKhalifa1 <enghamzakha@gmail.com>
2024-12-22 22:56:06 +0200

    remove any unnecessary code.

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Header, Footer } from '../Componant/Header_Footer';
 import Main_Title from "../Componant/Main_Title";
-import {Outlet,NavLink}  from 'react-router-dom';
-import styles from './Layout.module.css'
+import {Outlet}  from 'react-router-dom';
 
 
 const Layout     = () => {
@@ -11,14 +10,6 @@ const Layout     = () => {
             <Header/>
             <Main_Title/>
             <Outlet/>
-            <section className={styles.section}>
-                <NavLink to="/" className={styles.link}>
-                    HomePage
-                </NavLink>
-                <NavLink to="/CreateNewBlog" className={styles.link}>
-                    Home
-                </NavLink>
-            </section>
             <Footer/>
         </div>
     );
