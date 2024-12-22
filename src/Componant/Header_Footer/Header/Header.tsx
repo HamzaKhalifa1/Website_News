cd07aff9c39baf4eba7c09fb8e0171d1a1bfa8f8
HamzaKhalifa1 <enghamzakha@gmail.com>
2024-12-22 22:54:00 +0200

    add the pages and remove any unnecessary code.

--- src/Componant/Header_Footer/Header/Header.tsx
+++ src/Componant/Header_Footer/Header/Header.tsx
@@ -1,6 +1,7 @@
 import logo from "../../../assets/images/logo.png";
 import React from "react";
 import styles from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 
 export default function Header (){
@@ -9,41 +10,14 @@ export default function Header (){
             <div id={styles.div_img} >
                 <a href="./index.html"><img src={logo} alt="LOGO"/></a>
             </div>
-            <div id={styles.nav_sub}>
-                <ol className={styles.nav_info}>
-                    <li className={styles.class_info}>
-                        <select className={styles.select}>
-                            <option selected>CATEGORIES</option>
-                            <option >CATEGORIES1</option>
-                            <option >CATEGORIES2</option>
-                            <option >CATEGORIES3</option>
-                        </select>
-                    </li>
-                    <li className={styles.class_info}>
-                        <select className={styles.select}>
-                            <option selected>DEALS</option>
-                            <option >DEALS1</option>
-                            <option >DEALS2</option>
-                            <option >DEALS3</option>
-                        </select>
-                    </li>
-                    <li className={styles.class_info}>
-                        <a href="">ABOUT</a>
-                    </li>
-                    <li className={styles.class_info}>
-                        <a href="">ADVERTISE</a>
-                    </li>
-                </ol>
-            </div>
-            <div className={styles['dropdown-container']}>
-                <button className={styles['menu-button']}><svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#f7f7f8" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg></button>
-                <div className={styles['dropdown-menu']}>
-                    <a href="#">CATEGORIES</a>
-                    <a href="#">DEALS</a>
-                    <a href="#">ABOUT</a>
-                    <a href="#">ADVERTISE</a>
-                </div>
-            </div>
+            <section className={styles.section}>
+                <NavLink to="/" className={styles.link}>
+                    HomePage
+                </NavLink>
+                <NavLink to="/CreateNewBlog" className={styles.link}>
+                    Create new Blogs
+                </NavLink>
+            </section>
         </header >
     );
 }
