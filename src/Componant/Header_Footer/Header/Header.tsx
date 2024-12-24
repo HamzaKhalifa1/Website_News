8b6d345b0f2b13e20b6dc8b7f89f0a89ff74e1f4
TODO list <engHamzaKha@gmail.com>
2024-12-24 11:09:22 +0200

    refactor: replace anchor tag with React Router Link for logo navigation

--- src/Componant/Header_Footer/Header/Header.tsx
+++ src/Componant/Header_Footer/Header/Header.tsx
@@ -1,14 +1,14 @@
 import logo from "../../../assets/images/logo.png";
 import React from "react";
 import styles from './Header.module.css'
-import {NavLink} from "react-router-dom";
+import {NavLink,Link} from "react-router-dom";
 
 
 export default function Header (){
     return (
         <header id={styles.nav} >
             <div id={styles.div_img} >
-                <a href="./index.html"><img src={logo} alt="LOGO"/></a>
+                <Link to={'/'}><img src={logo} alt="LOGO"/></Link>
             </div>
             <section className={styles.section}>
                 <NavLink to="/" className={styles.link}>
