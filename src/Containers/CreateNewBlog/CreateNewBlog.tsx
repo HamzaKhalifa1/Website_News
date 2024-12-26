8f225de430621b480e130173d25c03dae18aa303
TODO list <engHamzaKha@gmail.com>
2024-12-26 11:32:28 +0200

    Enhance blog creation form with dynamic validation and loader integration

--- src/Containers/CreateNewBlog/CreateNewBlog.tsx
+++ src/Containers/CreateNewBlog/CreateNewBlog.tsx
@@ -3,6 +3,7 @@ import styles from './CreateNewBlog.module.css';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
+import {useLoader} from "../../Contexts/LoaderContext/LoaderContext";
 
 const CreateNewBlog = () => {
     const { showLoader, hideLoader } = useLoader();
@@ -16,6 +17,7 @@ const CreateNewBlog = () => {
             description: data.description,
             imageUrl: data.imageUrl,
         };
+        showLoader();
         axios.post(`http://localhost:8000/blogs`, formattedData)
             .then(() => {
                 setTimeout(() => hideLoader(), 1000);
@@ -33,6 +35,7 @@ const CreateNewBlog = () => {
                 className={styles[`title-input ${errors.title ? "error" : ""}`]}
                 placeholder="Enter title"
                 {...register("title", { required: true, pattern: /^[A-Z]/, maxLength: 50 })}
+                onKeyUp={() => trigger("title")}
             />
             <div id={styles['title-error']} className={styles['error-message']}>
                 {errors.title?.type === "required" && "Title is required."}
@@ -46,6 +49,7 @@ const CreateNewBlog = () => {
                 className={styles[`textarea-input ${errors.description ? "error" : ""}`]}
                 placeholder="Enter description"
                 {...register("description", { required: true, pattern: /^[a-zA-Z ]*$/, maxLength: 1000 })}
+                onKeyUp={() => trigger("description")}
             ></textarea>
             <div id={styles["description-error"]} className={styles['error-message']}>
                 {errors.description?.type === "required" && "Description is required."}
@@ -60,6 +64,7 @@ const CreateNewBlog = () => {
                 className={styles[`image-input ${errors.imageUrl ? "error" : ""}`]}
                 placeholder="Enter image URL"
                 {...register("imageUrl", { pattern: /^(https?:\/\/[^\s]+)$/ })}
+                onKeyUp={() => trigger("imageUrl")}
             />
             <div id={styles["image-url-error"]} className={styles['error-message']}>
                 {errors.imageUrl?.type === "pattern" && "Please enter a valid URL."}
