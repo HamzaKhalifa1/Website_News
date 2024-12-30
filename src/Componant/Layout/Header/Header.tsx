99b3772f296eab5dec71b287e742d6f92afb2556
HamzaKhalifa1 <enghamzakha@gmail.com>
2024-12-30 14:23:49 +0200

    feat: add language switcher with i18next in Header component

--- src/Componant/Layout/Header/Header.tsx
+++ src/Componant/Layout/Header/Header.tsx
@@ -2,9 +2,18 @@ import logo from "../../../assets/images/logo.png";
 import React from "react";
 import styles from './Header.module.css'
 import {NavLink,Link} from "react-router-dom";
+import { useTranslation } from 'react-i18next';
+import i18n from "i18next";
 
 
 export default function Header (){
+    const {t} = useTranslation();
+
+    const handleLanguageChange = (lang:string) => {
+        i18n.changeLanguage(lang);
+        window.document.dir=i18n.dir(lang);
+    };
+
     return (
         <header id={styles.nav} >
             <div id={styles.div_img} >
@@ -14,10 +23,16 @@ export default function Header (){
                 <NavLink to="/" className={styles.link}>
                     HomePage
                 </NavLink>
-                <NavLink to="/CreateNewBlog" className={styles.link}>
+                <NavLink to="CreateNewBlog" className={styles.link}>
                     Create new Blogs
                 </NavLink>
-
+                <div className={styles.dropdown}>
+                    <button className={styles.dropbtn}>{t("Language")}</button>
+                    <div className={styles.dropdownContent}>
+                        <button onClick={() => handleLanguageChange('en')}>English</button>
+                        <button onClick={() => handleLanguageChange('ar')}>العربية</button>
+                    </div>
+                </div>
             </section>
         </header>
     );
