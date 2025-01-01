4258961135b9ae6af669656f2c0bfd39622fef1c
TODO list <engHamzaKha@gmail.com>
2025-01-01 14:49:54 +0200

    Add loader integration and Redux state handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import PagesRoutes from "./Routes/PagesRoutes";
+import { useSelector } from 'react-redux';
+import { RootState } from './Store/Store';
+import Loader from './common/Loader';
 
 
 function App() {
+    const loader = useSelector((state: RootState) => state.loader.isLoading);
+
     return (
+        <>
+            {loader && <Loader />}
             <PagesRoutes/>
+        </>
+
     );
 }
 
