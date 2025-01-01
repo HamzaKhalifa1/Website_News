5a0678eda1d07fdba8f07c6826381ba9b2a50e63
TODO list <engHamzaKha@gmail.com>
2025-01-01 15:03:08 +0200

    feat: Add Redux slice for managing loader state

---
+++ src/Store/LoaderSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type LoaderState = {
+    isLoading : boolean;
+}
+const initialState : LoaderState = {
+    isLoading : false 
+}
+const LoaderSlice = createSlice({
+    name: 'loader',
+    initialState,
+    reducers: {
+        showLoader: (state) => {
+            state.isLoading = true;
+        },
+        hideLoader: (state) => {
+            state.isLoading = false;
+        },
+    },
+});
+
+export const { showLoader, hideLoader } = LoaderSlice.actions;
+export default LoaderSlice.reducer;
