2b1224bfd2bb54b6d01cd1492a450ce38bb9d05f
TODO list <engHamzaKha@gmail.com>
2025-01-01 15:00:50 +0200

    feat: Add language-based data handling in PageRoutes loader

--- src/Routes/PagesRoutes.tsx
+++ src/Routes/PagesRoutes.tsx
@@ -4,14 +4,17 @@ import HomePage from "../Pages/HomePage";
 import axios from "axios";
 import CreateNewBlog from "../Pages/CreateNewBlog";
 import React from "react";
+import cookies from "js-cookie";
 
 const PagesRoutes=()=>{
     const route = createBrowserRouter([
         {path:'/',element:<Layout/>,
             children:[
                 {index:true,element:<HomePage/>,loader: async ():Promise<any>=> {
+                        const lng = cookies.get('i18next') || 'en';
                         const response = await axios.get(`http://localhost:8000/blogs`);
-                        return response.data;
+                        const data = response.data;
+                        return data[lng.toString()] || [];
                     }
                 },
                 {path:'CreateNewBlog',element:<CreateNewBlog/>},
