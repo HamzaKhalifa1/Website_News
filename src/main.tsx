7cb97b692ac453d393ba8a7d8ca2de57801f1e5b
TODO list <engHamzaKha@gmail.com>
2025-01-01 15:04:42 +0200

    feat: Integrate Redux store with React app

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App';
 import './index.css';
 import './common/i18next/i18n';
+import {Provider} from 'react-redux';
+import Store from "./Store/Store";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-         <App />
-  </StrictMode>
+         <StrictMode>
+              <Provider store={Store}>
+              <App />
+              </Provider>
+         </StrictMode>
 
 )
