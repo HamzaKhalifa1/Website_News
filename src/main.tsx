fddac928bd403bd8327568f9c6a199fada2436d0
HamzaKhalifa1 <enghamzakha@gmail.com>
2025-01-07 16:05:41 +0200

    fix: resolve case sensitivity issues in file imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import './index.css';
-import './common/i18next/i18n';
+import './i18n';
 import {Provider} from 'react-redux';
-import Store from "./Store/Store";
+import Store from "./store/Store";
 
 createRoot(document.getElementById('root')!).render(
          <StrictMode>
