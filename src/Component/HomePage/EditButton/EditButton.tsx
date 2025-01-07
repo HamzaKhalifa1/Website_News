50fde31b141ea3d0bc9c0e55f344352d41919795
HamzaKhalifa1 <enghamzakha@gmail.com>
2025-01-07 16:12:19 +0200

    feat: implement edit button with navigation and loader handling

---
+++ src/Component/HomePage/EditButton/EditButton.tsx
@@ -0,0 +1,33 @@
+import styles from './EditButton.module.css';
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {hideLoader, showLoader} from "../../../store/LoaderSlice";
+import {useDispatch} from "react-redux";
+
+interface EditButtonProps {
+    id?: number
+}
+
+const EditButton = ({id}: EditButtonProps) => {
+    const [isVisited, setVisited] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(showLoader());
+        setTimeout(() => {
+            dispatch(hideLoader());
+        }, 1000);
+        setVisited(true);
+        navigate('CreateEditBlog', {state: {isVisited: true, id}});
+    };
+
+    return (
+        <i
+            onClick={handleClick}
+            className={`${styles.icon} fa fa-edit icon`}
+        ></i>
+    );
+}
+
+export default EditButton;
